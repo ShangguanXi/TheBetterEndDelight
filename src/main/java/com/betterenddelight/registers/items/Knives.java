667d84b0422cb55b9ae667d9c861f0daf0d96a2e
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2025-01-12 20:41:33 +0800

    新增刀

--- src/main/java/com/betterenddelight/registers/items/Knives.java
+++ src/main/java/com/betterenddelight/registers/items/Knives.java
@@ -11,6 +11,8 @@ import static vectorwing.farmersdelight.common.registry.ModItems.basicItem;
 
 public class Knives {
     public static final Item THALLASIUM_KNIFE = register("thallasium_knife",new KnifeItem(EndToolMaterial.THALLASIUM,0.5F, -2.0F, basicItem()));
+    public static final Item TERMINITE_KNIFE = register("terminite_knife",new KnifeItem(EndToolMaterial.TERMINITE,0.5F, -2.0F, basicItem()));
+    public static final Item AETERNIUM_KNIFE = register("aeternium_knife",new KnifeItem(EndToolMaterial.AETERNIUM,0.5F, -2.0F, basicItem()));
 
     public static <T extends Item> T register(String path, T item) {
         return BETTER_END_DELIGHT_GROUP.register(Registries.ITEM, makeId(path), item);
