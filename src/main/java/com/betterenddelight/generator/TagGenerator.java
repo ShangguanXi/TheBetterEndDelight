2ea7b31b26048eb10a0b8434235ee2dc6703489a
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2025-01-13 17:14:39 +0800

    新增tag生成

---
+++ src/main/java/com/betterenddelight/generator/TagGenerator.java
@@ -0,0 +1,19 @@
+package com.betterenddelight.generator;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class TagGenerator extends FabricTagProvider {
+    public TagGenerator(FabricDataOutput output, RegistryKey registryKey, CompletableFuture registriesFuture) {
+        super(output, registryKey, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+
+    }
+}
